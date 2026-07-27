0b661b9392a27d77586c485355ca35fe246d1a9a
Vinay Parit <vinaypparit@gmail.com>
2026-07-27 20:20:36 +0530

    New DOM Task Done

--- DOM Intro/onclick/script.js
+++ DOM Intro/onclick/script.js
@@ -16,43 +16,44 @@
 
 
 
-let form = document.querySelector("form")
-let inputs = document.querySelectorAll("input")
-let main = document.querySelector("#main")
+// let form = document.querySelector("form")
+// let inputs = document.querySelectorAll("input")
+// let main = document.querySelector("#main")
 
-form.addEventListener("submit", function(det){
-det.preventDefault();
-// console.log(inputs);
+// form.addEventListener("submit", function(det){
+// det.preventDefault();
+// // console.log(inputs);
 
-let card = document.createElement("div")
-card.classList.add("card")
+// let card = document.createElement("div")
+// card.classList.add("card")
 
-let profile = document.createElement("div")
-profile.classList.add("profile");
+// let profile = document.createElement("div")
+// profile.classList.add("profile");
 
-let img = document.createElement("img")
-img.setAttribute(
-  "src", inputs[0].value 
-);
+// let img = document.createElement("img")
+// img.setAttribute(
+//   "src", inputs[0].value 
+// );
 
-let h3 = document.createElement("h3")
-h3.textContent = inputs[1].value
-let h5 = document.createElement("h5")
-h5.textContent = inputs[2].value
-let p = document.createElement("p")
-p.textContent = inputs[3].value
+// let h3 = document.createElement("h3")
+// h3.textContent = inputs[1].value
+// let h5 = document.createElement("h5")
+// h5.textContent = inputs[2].value
+// let p = document.createElement("p")
+// p.textContent = inputs[3].value
 
-profile.appendChild(img);
-card.appendChild(profile);
-card.appendChild(h3)
-card.appendChild(h5)
-card.appendChild(p)
+// profile.appendChild(img);
+// card.appendChild(profile);
+// card.appendChild(h3)
+// card.appendChild(h5)
+// card.appendChild(p)
 
-main.appendChild(card)
+// main.appendChild(card)
 
-inputs.forEach(function(inp){
-    if(inp.type !== "submit")
-        inp.value = ""
-})
+// inputs.forEach(function(inp){
+//     if(inp.type !== "submit")
+//         inp.value = ""
+// })
 
-})
+// })
+
