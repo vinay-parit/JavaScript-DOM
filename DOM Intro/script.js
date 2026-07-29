84fcb20ddd11ebf6ab9cd3322e7e1ea9dbd65f9d
Vinay Parit <vinaypparit@gmail.com>
2026-07-29 23:39:56 +0530

    script.js updated

--- DOM Intro/script.js
+++ DOM Intro/script.js
@@ -2,9 +2,9 @@ let h1 = document.querySelector("h1");
 h1.innerHTML = `<ul><li>breakfast</li><li>lunch</li></ul>`;
 console.log(h1);
 
-h1.innerText="riya"
-h1.textContent="riya"
-h1.style.color="grey"
+// h1.innerText="riya"
+// h1.textContent="riya"
+// h1.style.color="grey"
 
 let obj={
     name:"hacker"
