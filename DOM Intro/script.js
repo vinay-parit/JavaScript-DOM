c977a1588f57b5fbf189017deac62ee4c8dc916d
Vinay Parit <vinaypparit@gmail.com>
2026-07-31 22:01:52 +0530

    Email Regex added

--- DOM Intro/script.js
+++ DOM Intro/script.js
@@ -2,3 +2,9 @@ let form = document.querySelector("form")
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 
+form.addEventListener("submit", function(dets){
+  dets.preventDefault();
+
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+   
+})
