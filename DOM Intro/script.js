1c1de166e593d2cbf269f4438961f76d92228d0f
Vinay Parit <vinaypparit@gmail.com>
2026-07-31 22:02:52 +0530

    Pass Regex added!!

--- DOM Intro/script.js
+++ DOM Intro/script.js
@@ -6,5 +6,5 @@ form.addEventListener("submit", function(dets){
   dets.preventDefault();
 
   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
-   
+  const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#()_+\-=\[\]{};':"\\|,.<>\/~`])[A-Za-z\d@$!%*?&^#()_+\-=\[\]{};':"\\|,.<>\/~`]{8,}$/
 })
