aa959a8098f93f30d19762e579af5e89f0e53df5
Vinay Parit <vinaypparit@gmail.com>
2026-08-01 16:12:45 +0530

    Script.js Updated

--- DOM Intro/script.js
+++ DOM Intro/script.js
@@ -5,33 +5,33 @@ let password = document.querySelector("#password");
 form.addEventListener("submit", function(dets){
   dets.preventDefault();
 
-    document.querySelector("#emailError").textContent = "";
-    document.querySelector("#passwordError").textContent = "";
+  document.querySelector("#emailError").textContent = "";
+  document.querySelector("#passwordError").textContent = "";
 
-
-  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   const passRegex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{8,}$/;
 
-    let isValid = true
+  let isValid = true;
 
-  let emailans = emailRegex.test(email.value)
-  let passwordans = emailRegex.test(password.value);
+  let emailans = emailRegex.test(email.value);
+  let passwordans = passRegex.test(password.value);
 
   if (!emailans) {
-    document.querySelector("#emailError").textContent = "Email is incorrect"
+    document.querySelector("#emailError").textContent = "Email is incorrect";
     document.querySelector("#emailError").style.display = "initial";
-    isValid = false
+    isValid = false;
   }
 
   if (!passwordans) {
-    document.querySelector("#passwordError").textContent = "Password is incorrect";
+    document.querySelector("#passwordError").textContent =
+      "Password is incorrect";
     document.querySelector("#passwordError").style.display = "initial";
     isValid = false;
   }
 
-  if(isValid){
-    document.querySelector("#resultMsg").textContent = "Everything is Correct"
+  if (isValid) {
+    document.querySelector("#resultMsg").textContent = "Everything is Correct";
+    document.querySelector("body").style.backgroundColor = "green";
   }
-
 })
