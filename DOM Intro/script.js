0251e430b1da16c3f5cb4feee19edc4c0313d6ba
Vinay Parit <vinaypparit@gmail.com>
2026-09-10 23:53:56 +0530

    Script.js updated

--- DOM Intro/script.js
+++ DOM Intro/script.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", function(dets){
   let isValid = true;
 
   let emailans = emailRegex.test(email.value);
-  let passwordans = passRegex.test(password.value);
+  let passwordans = passRegex.test(password);
 
   if (!emailans) {
     document.querySelector("#emailError").textContent = "Email is incorrect";
